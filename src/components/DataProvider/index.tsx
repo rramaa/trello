03c4e1aab0fe59886605ae0aad5d144c355d6d63
Ramakrishna <ramakrishna@mindtickle.com>
2022-09-11 13:48:19 +0530

    added functionality of initialising the application

--- src/components/DataProvider/index.tsx
+++ src/components/DataProvider/index.tsx
@@ -1,7 +1,10 @@
 import React, {PropsWithChildren} from "react";
-import {Board} from "../../data-models/board";
+import {Board, createBoard} from "../../data-models/board";
 import {Column} from "../../data-models/column";
 import {Card} from "../../data-models/card";
+import {generateId} from "../../helpers/entityHelper";
+import {storeData} from "../../helpers/storage";
+import {DATA_KEY, SELECTED_BOARD_ID_KEY} from "../../constants";
 
 export interface BoardData {
     BOARDS: Board[],
@@ -16,6 +19,16 @@ interface DataProvider extends React.FC<PropsWithChildren<{data: BoardData}>> {}
 export const Context = React.createContext<BoardData>(defaultData)
 
 const DataProvider: DataProvider = ({children, data}) => {
+    if(data.BOARDS.length === 0) {
+        console.log("Initializing Application")
+        let board = createBoard(generateId(), {name: "Default Board"})
+        data.BOARDS.push(board)
+        const selectedBoardId = {
+            id: board.id
+        }
+        storeData(DATA_KEY, data)
+        storeData(SELECTED_BOARD_ID_KEY, selectedBoardId)
+    }
     return <Context.Provider value={data}>{children}</Context.Provider>
 }
 
